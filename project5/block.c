5d5b90a41fc219be77ef845c7ad11a2a876f504c
orion-junkins <orionjunkins@gmail.com>
2023-05-01 16:53:51 -0600

    Block reading and writing added

--- project5/block.c
+++ project5/block.c
@@ -1,11 +1,28 @@
-unsigned char *bread(int block_num, unsigned char *block);
-/*
-Take a block number and a pointer to a block-sized unsigned char buffer to load the data into. For convenience, it should also return block at the end.
-*/
-void bwrite(int block_num, unsigned char *block);
-/*
-bwrite() takes a block number and a pointer to the data to write.
-*/
+#include <stdio.h>
+#include <unistd.h>
+
+#include "block.h"
+#include "image.h"
+#define BLOCK_SIZE 4096
+unsigned char *bread(int block_num, unsigned char *block)
+{
+    /*
+    Take a block number and a pointer to a block-sized unsigned char buffer to load the data into. For convenience, it should also return block at the end.
+    */
+    // Use the lseek() system call to move to the proper place within the image before you call read() or write() to load or save the block.
+    lseek(image_fd, block_num * BLOCK_SIZE, SEEK_SET);
+    read(image_fd, block, BLOCK_SIZE);
+
+    return block;
+}
+void bwrite(int block_num, unsigned char *block)
+{
+    /*
+    Takes a block number and a pointer to the data to write.
+    */
+    lseek(image_fd, block_num * BLOCK_SIZE, SEEK_SET);
+    write(image_fd, block, BLOCK_SIZE);
+}
 
 int alloc(void);
 /*allocate a previous-free data block from the block map.*/
--- project5/simfs_test.c
+++ project5/simfs_test.c
@@ -1,15 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 #include "ctest.h"
 #include "image.h"
+#include "block.c"
 
 void test_file_creation(void)
 {
     image_open("image", 0);
+    CTEST_ASSERT(image_fd != -1, "testing file is open");
     image_close();
     CTEST_ASSERT(access("image", F_OK) == 0, "testing file creation");
+}
+
+void test_bread_and_bwrite(void)
+{
+    image_open("image", 1);
+    unsigned char *block_a_to_write = malloc(BLOCK_SIZE);
+    for (int i = 0; i < BLOCK_SIZE; i++)
+    {
+        block_a_to_write[i] = 'a';
+    }
+    bwrite(1, block_a_to_write);
+
+    unsigned char *block_b_to_write = malloc(BLOCK_SIZE);
+    for (int i = 0; i < BLOCK_SIZE; i++)
+    {
+        block_b_to_write[i] = 'b';
+    }
+    bwrite(2, block_b_to_write);
+
+    unsigned char *block_a_to_read = malloc(BLOCK_SIZE);
+    bread(1, block_a_to_read);
+
+    unsigned char *block_b_to_read = malloc(BLOCK_SIZE);
+    bread(2, block_b_to_read);
+
+    CTEST_ASSERT(memcmp(block_a_to_read, block_a_to_write, BLOCK_SIZE) == 0, "testing bread and bwrite");
+    CTEST_ASSERT(memcmp(block_b_to_read, block_b_to_write, BLOCK_SIZE) == 0, "testing bread and bwrite");
+    CTEST_ASSERT(memcmp(block_a_to_read, block_b_to_write, BLOCK_SIZE) != 0, "testing bread and bwrite");
+    image_close();
 }
 
 int main(void)
@@ -17,6 +49,7 @@ int main(void)
     CTEST_VERBOSE(1);
 
     test_file_creation();
+    test_bread_and_bwrite();
 
     CTEST_RESULTS();
 
